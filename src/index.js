13b315ea1f370df592fbacad62f96ab77056ddb7
axcasella <axc@bu.edu>
2023-10-03 01:33:24 -0400

    persisted store to local storage

--- src/index.js
+++ src/index.js
@@ -8,13 +8,25 @@ import { Provider } from "react-redux";
 import { api } from "state/api";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
+// Load the persisted state from localStorage
+const persistedState = localStorage.getItem('reduxState')
+  ? JSON.parse(localStorage.getItem('reduxState'))
+  : {};
+
 const store = configureStore({
   reducer: {
     global: globalReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefault) => getDefault().concat(api.middleware),
+  preloadedState: persistedState,  // Provide the persisted state here
 });
+
+// Save the current state to localStorage whenever it changes
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+});
+
 setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
